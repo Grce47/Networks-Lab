d217105affbb5e8509d11ebe901ea97517603b01
grce47 <gracesharma47@gmail.com>
2023-01-19 09:46:59 +0530

    Thu 19 Jan 09:46:59 IST 2023

--- Assignment2/part1/timeclient.c
+++ Assignment2/part1/timeclient.c
@@ -1,39 +1,88 @@
 // A Simple Client Implementation
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-  
-int main() { 
-    int sockfd; 
-    struct sockaddr_in servaddr; 
-  
-    // Creating socket file descriptor 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <netinet/in.h>
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if ( sockfd < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-  
-    memset(&servaddr, 0, sizeof(servaddr)); 
-      
-    // Server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(8181); 
-    inet_aton("127.0.0.1", &servaddr.sin_addr); 
-      
+    if (sockfd < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8181);
+    inet_aton("127.0.0.1", &servaddr.sin_addr);
+
+    struct pollfd fds;
+    fds.fd = sockfd;
+    fds.events = POLLIN;
+
+    int success = 0;
     int n;
-    socklen_t len; 
-    char *hello = "CLIENT:HELLO"; 
-      
-    sendto(sockfd, (const char *)hello, strlen(hello), 0, 
-			(const struct sockaddr *) &servaddr, sizeof(servaddr)); 
-    printf("Hello message sent from client\n"); 
-           
-    close(sockfd); 
-    return 0; 
-} 
+    socklen_t len;
+    char *hello = "CLIENT:HELLO";
+    const int timeout = 3000, MAXLINE = 1024;
+    char buffer[MAXLINE];
+
+    for (int i = 0; i < 5; i++)
+    {
+        // Sending Message to Server
+        sendto(sockfd, (const char *)hello, strlen(hello), 0,
+               (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+        int ret = poll(&fds, 1, timeout);
+        if (ret == -1)
+        {
+            perror("Error in poll()");
+            exit(1);
+        }
+        else if (ret == 0)
+        {
+            printf("Timeout: Waiting for data\n");
+        }
+        else
+        {
+            // Receive data from server
+            len = sizeof(servaddr);
+            n = recvfrom(sockfd, (char *)buffer, MAXLINE, 0,
+                         (struct sockaddr *)&servaddr, &len);
+            buffer[n] = '\0';
+
+            if (n < 0)
+            {
+                perror("Error receiving data");
+                exit(1);
+            }
+            else
+            {
+                printf("Received : %s\n", buffer);
+                success = 1;
+                break;
+            }
+        }
+    }
+
+    if (!success)
+    {
+        printf("Failed 5 Times!!\n");
+    }
+
+    close(sockfd);
+    return 0;
+}
--- Assignment2/part1/timeserv.c
+++ Assignment2/part1/timeserv.c
@@ -1,52 +1,74 @@
 // A Simple UDP Server that sends a HELLO message
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-  
-#define MAXLINE 1024 
-  
-int main() { 
-    int sockfd; 
-    struct sockaddr_in servaddr, cliaddr; 
-      
-    // Create socket file descriptor 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Create socket file descriptor
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if ( sockfd < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    memset(&servaddr, 0, sizeof(servaddr)); 
-    memset(&cliaddr, 0, sizeof(cliaddr)); 
-      
-    servaddr.sin_family    = AF_INET; 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(8181); 
-      
-    // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
-            sizeof(servaddr)) < 0 ) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    
+    if (sockfd < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(8181);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr,
+             sizeof(servaddr)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
     printf("\nServer Running....\n");
-  
-    int n; 
+
+    const int MAXLINE = 1024;
+    int n;
     socklen_t len;
-    char buffer[MAXLINE]; 
- 
-    len = sizeof(cliaddr);
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 0, 
-			( struct sockaddr *) &cliaddr, &len); 
-    buffer[n] = '\0'; 
-    printf("%s\n", buffer); 
+    char buffer[MAXLINE];
+    while (1)
+    {
+        // Recieve Message
+        {
+            len = sizeof(cliaddr);
+            n = recvfrom(sockfd, (char *)buffer, MAXLINE, 0,
+                         (struct sockaddr *)&cliaddr, &len);
+            buffer[n] = '\0';
+            printf("%s\n", buffer);
+        }
+
+        // Sending Message
+        {
+            time_t rawtime;
+            struct tm *timeinfo;
+
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
+
+            strcpy(buffer, "Server time and date: ");
+            strcat(buffer, asctime(timeinfo));
+
+            sendto(sockfd, (const char *)buffer, strlen(buffer), 0,
+                   (const struct sockaddr *)&cliaddr, sizeof(cliaddr));
+        }
+    }
     close(sockfd);
-    return 0; 
-} 
+    return 0;
+}
